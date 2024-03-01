c8bd104d550779d9d25c39f8b10a47b2c592b1e3
SPatil2026 <153353355+SPatil2026@users.noreply.github.com>
2024-03-01 12:15:28 +0530

    Update Location.py

--- Location.py
+++ Location.py
@@ -1,1 +1,10 @@
-print("Hello")
+import PIL.Image
+import PIL.ExifTags
+
+img = PIL.Image.open("")
+
+exif = {
+    PIL.ExifTags.TAGS[k]: v
+    for k, v in img._getexif().items()
+    if k in PIL.ExifTags.TAGS
+}
