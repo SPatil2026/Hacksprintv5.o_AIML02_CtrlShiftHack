71d81b3ce926000e388398d26faf1171c3859e33
SPatil2026 <153353355+SPatil2026@users.noreply.github.com>
2024-03-01 12:34:17 +0530

    Update Location.py

--- Location.py
+++ Location.py
@@ -1,10 +1,14 @@
 import PIL.Image
 import PIL.ExifTags
 
-img = PIL.Image.open("")
+img = PIL.Image.open("Test.jpg")
 
-exif = {
-    PIL.ExifTags.TAGS[k]: v
-    for k, v in img._getexif().items()
-    if k in PIL.ExifTags.TAGS
-}
+if hasattr(img, "_getexif") and callable(getattr(img, "_getexif")):
+    exif = {
+        PIL.ExifTags.TAGS[k]: v
+        for k, v in img._getexif().items()
+        if k in PIL.ExifTags.TAGS
+    }
+    print(exif)
+else:
+    print("NO data Found!")
